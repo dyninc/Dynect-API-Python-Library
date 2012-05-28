50d91d70a57f63ad9c0345702fad3788e5f16d73
Cole Tuininga <ctuininga@heartofgold.(none)>
2012-05-27 22:35:26 -0400

    Be more in line with proper Python packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from distutils.core import setup
 
 # sync with __init__.py
-version = '0.1'
+version = '0.2'
 
 setup(
     name="DynectDNS",
@@ -10,6 +10,13 @@ setup(
     keywords=["dynect", "api", "dns"],
     long_description=open(os.path.join(os.path.dirname(__file__),"README"), "r").read(),
     description="Dynect REST API wrapper",
-    author="dyn.com",
+    author="Cole Tuininga",
+    author_email="ctuininga@dyn.com",
+    url="https://github.com/dyninc/Dynect-API-Python-Library",
     packages=['dynect'],
+    classifiers=[
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Software Development :: Libraries', 
+    ],
 )
